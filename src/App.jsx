4273a3b4aa20f57f9365e43afc3c53749c295ab8
Venkata Ranjit Tholupuluri <112625318+venkataranjit@users.noreply.github.com>
2024-11-21 18:30:30 +0530

    project created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Dashboard2 from "./Dashboard2";
+// import Login from "./Login";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <h3>Track My Money</h3>
+      {/* <Login /> */}
+
+      <Dashboard2 />
+    </>
+  );
+}
+
+export default App;
