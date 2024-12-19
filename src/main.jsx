08358fffca454c264d9af943c489386e814edc61
venkataranjit <venkataranjit@live.com>
2024-12-19 22:19:38 +0530

    registration form feilds save and store created

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./styles/index.css";
 import "material-icons/iconfont/material-icons.css";
 import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </StrictMode>
 );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
 import { Link } from "react-router-dom";
 import {
@@ -16,6 +17,21 @@ const Register = () => {
   React.useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    mobile: "",
+    captcha: "",
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <>
       <main className="d-flex w-100 login">
@@ -40,75 +56,121 @@ const Register = () => {
                       <div className="text-center">
                         <h4>Register</h4>
                       </div>
-                      <form>
-                        <div className="mb-3">
-                          <label className="form-label">Name</label>
-                          <input
-                            className="form-control form-control-lg"
-                            type="text"
-                            name="name"
-                            placeholder="Enter your Name"
-                          />
-                        </div>
-                        <div className="mb-3">
-                          <label className="form-label">Email</label>
-                          <input
-                            className="form-control form-control-lg"
-                            type="email"
-                            name="email"
-                            placeholder="Enter your email"
-                          />
-                        </div>
-                        <div className="mb-3">
-                          <label className="form-label">Password</label>
-                          <input
-                            className="form-control form-control-lg"
-                            type="password"
-                            name="password"
-                            placeholder="Enter your password"
-                          />
-                        </div>
-                        <div className="row">
-                          <div className="mb-1 col-sm-6">
-                            <label className="form-label">Captcha</label>
-                            <input
-                              className="form-control form-control-lg"
-                              type="text"
-                              name="captcha"
-                              placeholder="Enter Captcha"
-                            />
-                          </div>
-                          <div className="col-sm-6">
-                            <label className="form-label">&nbsp;</label>
-                            <div className="captcha-container">
-                              <LoadCanvasTemplateNoReload />
-                              <button
-                                className="btn btn-info"
-                                onClick={refreshCaptcha}
-                              >
-                                <span className="material-icons-round align-middle">
-                                  refresh
-                                </span>
-                              </button>
+                      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                        <Form>
+                          <div className="row">
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">First Name</label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="text"
+                                  name="firstName"
+                                  placeholder="Enter your Name"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">Last Name</label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="text"
+                                  name="lastName"
+                                  placeholder="Enter your Name"
+                                />
+                              </div>
+                            </div>
+
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="email"
+                                  name="email"
+                                  placeholder="Enter your email"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">Mobile</label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="number"
+                                  name="mobile"
+                                  placeholder="Enter your Mobile"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">Password</label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="password"
+                                  name="password"
+                                  placeholder="Enter password"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-sm-6">
+                              <div className="mb-3">
+                                <label className="form-label">
+                                  Confirm Password
+                                </label>
+                                <Field
+                                  className="form-control form-control-lg"
+                                  type="password"
+                                  name="cpassword"
+                                  placeholder="Confirm password"
+                                />
+                              </div>
                             </div>
                           </div>
-                        </div>
-                        <div className="block mb-5">
-                          {/* <small className="float-start">
+                          <div className="row">
+                            <div className="mb-1 col-sm-6">
+                              <label className="form-label">Captcha</label>
+                              <Field
+                                className="form-control form-control-lg"
+                                type="text"
+                                name="captcha"
+                                placeholder="Enter Captcha"
+                              />
+                            </div>
+                            <div className="col-sm-6">
+                              <label className="form-label">&nbsp;</label>
+                              <div className="captcha-container">
+                                <LoadCanvasTemplateNoReload />
+                                <button
+                                  className="btn btn-info"
+                                  onClick={refreshCaptcha}
+                                >
+                                  <span className="material-icons-round align-middle">
+                                    refresh
+                                  </span>
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="block mb-2">
+                            {/* <small className="float-start">
                             <Link to="/forgetPassword">Forgot password?</Link>
                           </small> */}
-                          <small className="float-end">
-                            Already Have an Account?
-                            <Link to="/login"> Login Here</Link>
-                          </small>
-                        </div>
+                            <small>
+                              Already Have an Account?
+                              <Link to="/login"> Login Here</Link>
+                            </small>
+                          </div>
 
-                        <div className="text-center mt-3">
-                          <button className="btn btn-lg btn-info">
-                            Register
-                          </button>
-                        </div>
-                      </form>
+                          <div className="text-center mt-3">
+                            <button className="btn btn-lg btn-info">
+                              Register
+                            </button>
+                          </div>
+                        </Form>
+                      </Formik>
                     </div>
                   </div>
                 </div>
