997b9ccf742208c72d571ca0ffc2da4538e324c8
Venkata Ranjit Tholupuluri <112625318+venkataranjit@users.noreply.github.com>
2025-01-11 00:06:28 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Track My Money",
+        short_name: "Track My Money",
+        description: "Track Daily Expenses",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "fullscreen",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
